ebf63158bae98455fb0eab5dfd5c03d91315f4a6
Saft-ByteCrafter <musiknoname@gmail.com>
2021-06-28 14:15:22 +0200

    added some more text, commit mainly to transfer changes to another device

--- SkribbelCircuit.ino
+++ SkribbelCircuit.ino
@@ -1,26 +1,37 @@
-int task = 1;
+int task = 0;
+#define ledPin 12 //TODO we can change that pot though
+#define delay 500
+
 
 void setup(){
   Serial.begin(9600);
 
   Serial.println("Hello and welcome to Scribble-Circuit!");
-  delay(500);
+  delay(delay);
   Serial.println("Here you can draw your own electric Circuits on paper!");
-  delay(500);
+  delay(delay);
   Serial.println("Just draw thick lines with the pen on a paper");
   Serial.println("and put the right components on it.");
-  delay(500);
+  delay(delay);
   Serial.println("Lastly, put the cables into the Arduino.");
-  delay(500);
+  delay(delay);
   Serial.println("Let's start now, shall we?");
-  delay(500);
+  delay(delay);
 }
 
 void loop(){
 
   switch (task)) {
-    case 1:
+    case 0:
       //port-setup if neccessary
+      Serial.println("First,  connect the black cable do a Gnd Port");
+      Serial.println("Then connect the red cable to port " + ledPin);
+      Serial.println("Then connect an led with the - part do the black cable");
+      Serial.println("and then connect the red cable to a resistor, which");
+      Serial.println("you want to connect to the + of the led.");
+      Serial.println();
+      Serial.println("When you are done, type \"done\" into the console.");
+      digitalWrite(ledPin, HIGH);
       while(true){
 
         //put stuff here
@@ -37,7 +48,7 @@ void loop(){
       task++;
       //^this at the end of this case
       break;
-    case 2:
+    case 1:
       while(true){
         //put stuff Here
         if("win condition")break;
