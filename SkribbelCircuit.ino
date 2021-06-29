d6eaab6eb4899c3160dcd045a9158f5964255a7a
Saft-ByteCrafter <musiknoname@gmail.com>
2021-06-29 10:21:39 +0200

    Some structural changes

--- SkribbelCircuit.ino
+++ SkribbelCircuit.ino
@@ -1,4 +1,5 @@
 int task = 0;
+bool newTask = true;
 #define ledPin 12 //TODO we can change that pot though
 #define textDelay 500
 
@@ -28,7 +29,7 @@ void loop(){
       //port-setup if neccessary
       Serial.println("First,  connect the black cable do a Gnd Port.");
       delay(textDelay);
-      Serial.println("Then connect the red cable to port " + ledPin + " .");
+      Serial.println("Then connect the red cable to port " + ledPin + ".");
       delay(textDelay);
       Serial.println("Then connect an led with the - part to the black cable");
       Serial.println("and then connect the red cable to a resistor, which");
@@ -38,43 +39,63 @@ void loop(){
       Serial.println("When you are done, type \"done\" into the console.");
       digitalWrite(ledPin, HIGH);
       while(true){
-        if(Serial.available() > 0){
-          String message = Serial.read();
-          message = message.tolower();
-          if(message.equalsIgnoreCase("done"))break;
-          delay(500);
-        }
+        if(checkInput(true))break;
+        delay(500);
       }
       Serial.println("By the way, you can jump to any task by just");
       Serial.println("sending the index of the task into the terminal!");
       delay(textDelay);
       Serial.println("You can also end the task you are working on right now");
       Serial.println("by typing \"end\" in the terminal.");
-      task++;
+      if(newTask) task++;
       //^this at the end of this case
       break;
     case 1:      //port-setup if neccessary
       Serial.println("First,  connect the black cable do a Gnd Port.");
       delay(textDelay);
-      Serial.println("Then connect the red cable to port " + ledPin + " .");
+      Serial.println("Then connect the red cable to port " + ledPin + ".");
       delay(textDelay);
       Serial.println("Then connect an led with the - part do the black cable");
       Serial.println("and then connect the red cable to a button, which");
       Serial.println("you want to connect to the resistor, ");
-      Serial.println("which you connect to the + of the led.")
+      Serial.println("which you connect to the + of the led.");
+      delay(textDelay);
+      Serial.println();
+      Serial.println("When you are done, type \"done\" into the console.");
       digitalWrite(ledPin, HIGH);
       while(true){
-          String message = Serial.read();
-          message = message.tolower();
-          if(message.equalsIgnoreCase("done"))break;
-          if(message.equalsIgnoreCase("end"))break;
-          if(message.equalsIgnoreCase("0")){
-            task = -1;
-            break;
-          }
+          if(checkInput(true))break;
           delay(500);
       }
-      task++;
-      break;
+      if(newTask) task++;
+        break;
   }
+}
+
+
+
+boolean checkInput(boolean doneYN){
+  String message = Serial.read();
+  message = message.tolower();
+  if(doneYN){
+    if(message.equalsIgnoreCase("done")){
+      newTask = true;
+      return true;
+    }
+  }
+  if(message.equalsIgnoreCase("end")){
+    newTask = false;
+    return true;
+  }
+  if(message.equalsIgnoreCase("0")){
+    newTask = false;
+    task = 0;
+    return true;
+  }
+  if(message.equalsIgnoreCase("1")){
+    newTask = false;
+    task = 1;
+    return true;
+  }
+  return false;
 }
