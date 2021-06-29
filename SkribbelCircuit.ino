3ff30a75b3df063135e9f2770ca34aa720081f75
HornNr1 <martin.schubert@iserv-gis.de>
2021-06-29 10:06:02 +0200

    Text delay changes

--- SkribbelCircuit.ino
+++ SkribbelCircuit.ino
@@ -1,6 +1,6 @@
 int task = 0;
 #define ledPin 12 //TODO we can change that pot though
-#define delay 500
+#define textDelay 500
 
 
 void setup(){
@@ -9,16 +9,16 @@ void setup(){
   Serial.begin(9600);
 
   Serial.println("Hello and welcome to Scribble-Circuit!");
-  delay(delay);
+  delay(textDelay);
   Serial.println("Here you can draw your own electric Circuits on paper!");
-  delay(delay);
+  delay(textDelay);
   Serial.println("Just draw thick lines with the pen on a paper");
   Serial.println("and put the right components on it.");
-  delay(delay);
+  delay(textDelay);
   Serial.println("Lastly, put the cables into the Arduino.");
-  delay(delay);
+  delay(textDelay);
   Serial.println("Let's start now, shall we?");
-  delay(delay);
+  delay(textDelay);
 }
 
 void loop(){
@@ -26,11 +26,14 @@ void loop(){
   switch (task)) {
     case 0:
       //port-setup if neccessary
-      Serial.println("First,  connect the black cable do a Gnd Port");
-      Serial.println("Then connect the red cable to port " + ledPin);
-      Serial.println("Then connect an led with the - part do the black cable");
+      Serial.println("First,  connect the black cable do a Gnd Port.");
+      delay(textDelay);
+      Serial.println("Then connect the red cable to port " + ledPin + " .");
+      delay(textDelay);
+      Serial.println("Then connect an led with the - part to the black cable");
       Serial.println("and then connect the red cable to a resistor, which");
       Serial.println("you want to connect to the + of the led.");
+      delay(textDelay);
       Serial.println();
       Serial.println("When you are done, type \"done\" into the console.");
       digitalWrite(ledPin, HIGH);
@@ -44,15 +47,17 @@ void loop(){
       }
       Serial.println("By the way, you can jump to any task by just");
       Serial.println("sending the index of the task into the terminal!");
-      delay(500);
+      delay(textDelay);
       Serial.println("You can also end the task you are working on right now");
       Serial.println("by typing \"end\" in the terminal.");
       task++;
       //^this at the end of this case
       break;
     case 1:      //port-setup if neccessary
-      Serial.println("First,  connect the black cable do a Gnd Port");
-      Serial.println("Then connect the red cable to port " + ledPin);
+      Serial.println("First,  connect the black cable do a Gnd Port.");
+      delay(textDelay);
+      Serial.println("Then connect the red cable to port " + ledPin + " .");
+      delay(textDelay);
       Serial.println("Then connect an led with the - part do the black cable");
       Serial.println("and then connect the red cable to a button, which");
       Serial.println("you want to connect to the resistor, ");
