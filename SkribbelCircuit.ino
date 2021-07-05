cf37ae285dd6c2c16e3263ed8d212075ea81e255
HornNr1 <martin.schubert@iserv-gis.de>
2021-07-05 18:10:12 +0200

    Andere Aufgaben hinzugefügt und die 1. und 2. angepasst

--- SkribbelCircuit.ino
+++ SkribbelCircuit.ino
@@ -9,7 +9,7 @@ void setup(){
   pinMode(ledPin, OUTPUT);
 
   Serial.begin(9600);
-
+  delay(3000);
   Serial.println("Hello and welcome to Scribble-Circuit!");
   delay(textDelay);
   Serial.println("Here you can draw your own electric Circuits on paper!");
@@ -36,8 +36,9 @@ void loop(){
       String text2 = ".";
       Serial.println(text1 + ledPin + text2);
       delay(textDelay);
-      Serial.println("Then connect an led with the - (short leg) part to the black cable");
-      Serial.println("and then connect the red cable to a resistor, which");
+      Serial.println("Then connect an both cables to two different snails and")
+      Serial.println("connect a led with the - (short leg) part to the black cable snail");
+      Serial.println("and then connect the red cable snail to a resistor, which");
       Serial.println("you want to connect to the + (long leg) of the led.");
       delay(textDelay);
       Serial.println();
@@ -65,8 +66,9 @@ void loop(){
       String text2 = ".";
       Serial.println(text1 + ledPin + text2);
       delay(textDelay);
-      Serial.println("Then connect an led with the - part do the black cable");
-      Serial.println("and then connect the red cable to a button, which");
+      Serial.println("Then you connect both cables to two snails and");
+      Serial.println("connect an led with the - part to the black cable sanil");
+      Serial.println("and then connect the red cable snail to a button, which");
       Serial.println("you want to connect to the resistor, ");
       Serial.println("which you connect to the + of the led.");
       delay(textDelay);
@@ -81,6 +83,117 @@ void loop(){
       if(newTask) task++;
       Serial.println();
         break;
+    }
+    case 2:{
+     int Light = 0;
+     Serial.println("First,  connect the black cable do a Gnd Port.");
+     delay(textDelay);
+     Serial.println("Then connect the red cable to port ");
+     Serial.println(ledPin);
+     Serial.println(".");
+     delay(textDelay);
+     Serial.println("After that, you connect both cables to the snale");
+     Serial.println("and then draw a line from the snale with the black cable");
+     Serial.println("to the potentiometer, which you connect than");
+     Serial.println("to the - part of the led.");
+     delay(textDelay);
+     Serial.println("When you have done this, you draw another line");
+     Serial.println("from the red cable snail to the + of the led.");
+     delay(textDelay);
+     Serial.println("When you are finished with the construction,");
+     Serial.println("you can try to spin the potentiometer and watch");
+     Serial.println("what happened.");
+     Serial.println();
+     Serial.println("When you are done, type \"done\" into the console.");
+     digitalWrite(ledPin, HIGH);
+     while(true){
+         if(checkInput(true))break;
+         delay(500);
+     }
+     if(newTask) task++;
+       break;
+   }
+   case 3:{
+     #define Buzzer 11
+     #define Taster_1 10
+     #define Tone_1 "C"
+     #define Taster_2 9
+     #define Tone_2 "D"
+     #define Taster_3 7
+     #define Tone_3 "E"
+     #define Taster_4 6
+     #define Tone_4 "F"
+     #define Taster_5 5
+     #define Tone_5 "G"
+ /*  #define Taster_6 4
+     #define Tone_6 A
+     #define Taster_7 3
+     #define Tone_7 H */
+     #define BuzzerTime 5
+     Serial.println("First,  connect the black cable do a Gnd Port.");
+     delay(textDelay);
+     Serial.println("Then connect the red cable to port 5V.");
+     delay(textDelay);
+     Serial.println("Third, connect 5 cables to the port 10 to 3.");
+     delay(textDelay);
+     Serial.println("After that, you connected all cables to the snale");
+     Serial.println("and then draw a line from the snale with the black cable");
+     Serial.println("to the buzzer.");
+     delay(textDelay);
+     Serial.println("When you have done that, you draw a long line");
+     Serial.println("from the snail with the red cable to the other");
+     Serial.println("side of the area.");
+     delay(textDelay);
+     Serial.println("Than you place all the buttons in a row ");
+     Serial.println("and draw a line from the snail with the cable of");
+     Serial.println("the 10. port to the left leg of the button.");
+     delay(textDelay);
+     Serial.println("Then you draw from the long line of the red cable");
+     Serial.println("to the right leg of the button.");
+     delay(textDelay);
+     Serial.println("Now connect all other buttons the same way,");
+     Serial.println("exept you connect the 9. port to the 2. button");
+     Serial.println("and so on.");
+     delay(textDelay);
+     Serial.println("When you are finished with the construction,");
+     Serial.println("you can try to press the buttons and watch");
+     Serial.println("what happened.");
+     delay(textDelay);
+     Serial.println();
+     Serial.println("When you are done, type \"done\" into the console.");
+     if(digitalRead(Taster_1) == HIGH) {
+       tone(Buzzer, Tone_1);
+       delay(BuzzerTime);
+     }else if(digitalRead(Taster_2) == HIGH) {
+       tone(Buzzer, Tone_2);
+       delay(BuzzerTime);
+     }else if(digitalRead(Taster_3) == HIGH) {
+       tone(Buzzer, Tone_3);
+       delay(BuzzerTime);
+     }else if(digitalRead(Taster_4) == HIGH) {
+       tone(Buzzer, Tone_4);
+       delay(BuzzerTime);
+     }else if(digitalRead(Taster_5) == HIGH) {
+       tone(Buzzer, Tone_5);
+       delay(BuzzerTime);
+/*    }else if(digitalRead(Taster_6) == HIGH) {
+       tone(Buzzer, Tone_6);
+       delay(BuzzerTime);
+     }else if(digitalRead(Taster_7) == HIGH) {
+       tone(Buzzer, Tone_7);
+       delay(BuzzerTime); */
+     }else {
+       noTone(Buzzer);
+       delay(BuzzerTime);
+     }
+
+     while(true){
+       delay(500);
+         if(Serial.available()) continue;
+         if(checkInput(true))break;
+     }
+     if(newTask) task++;
+       break;
     }
   }
 }
@@ -119,7 +232,7 @@ bool checkInput(bool doneYN, String message){
     newTask = true;
     return true;
   }
-  else if(x && num > 0 && num < 3){
+  else if(x && num > 0 && num < 5){
     newTask = false;
     task = num - 1;
     return true;
